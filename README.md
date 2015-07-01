063d9f42f2b837de439a7bcd1d7ff15362dc46e9
inertial.observer <student@epicodus.com>
2015-07-01 08:26:24 -0700

    pass integration specs and allow user to add more than one shoe brand to a store

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ Run ruby app.rb Navigate in your browser to localhost:4567
 
 Known Bugs
 
-"Error" not working
+Does redirect to "Error" page when a blank form is submitted from the home page, but not when blank forms are submitted from other pages as those paths are not written yet.
 
 Bug reports
 
--- spec/shoe_tracker_integration_spec.rb
+++ spec/shoe_tracker_integration_spec.rb
@@ -7,12 +7,28 @@ set(:show_exceptions, false)
 
 describe('Shoe Tracker', type: :feature) do
 
+  describe('Home', {:type => :feature}) do
+    it('displays expected home page content') do
+      visit('/')
+      page.should have_content("Welcome to the Shoe Database!")
+    end
+  end
+
   describe('add store') do
     it('adds a new store') do
       visit('/')
       fill_in('name', with: 'Cabella')
-      click_button('Add Store')
+      click_button('Add store')
       expect(page).to(have_content('Cabella'))
+    end
+  end
+
+  describe('add shoe') do
+    it('adds a new shoe') do
+      visit('/')
+      fill_in('brand', with: 'Nike')
+      click_button('Add shoe')
+      expect(page).to(have_content('Nike'))
     end
   end
 end
